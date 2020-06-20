4e61c936a0ef438af0615dea44d05c50a62c5f7d
Luis Cortes <lfcortes12@gmail.com>
2020-06-20 15:33:11 -0500

    adds spring service implementation

--- spring-order/src/main/java/com/gbt/cloud/springorder/web/GameScoreResource.java
+++ spring-order/src/main/java/com/gbt/cloud/springorder/web/GameScoreResource.java
@@ -1,24 +1,41 @@
 package com.gbt.cloud.springorder.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.gbt.cloud.springorder.model.Score;
 import com.gbt.cloud.springorder.repository.GameScoreRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
-@AllArgsConstructor
-@RestController("/game-score")
+@RefreshScope
+@RequestMapping(path = "/game-score")
+@RestController
 public class GameScoreResource {
 
-    private final GameScoreRepository gameScoreRepository;
+	private final GameScoreRepository gameScoreRepository;
 
-    @GetMapping(name = "/")
-    public ResponseEntity<List<Score>> getAll() {
-        return ResponseEntity.ok(gameScoreRepository.findAll());
-    }
+	private final String gameScoreKey;
+
+	public GameScoreResource(GameScoreRepository gameScoreRepository, @Value("${gamescore.key}") String gameScoreKey) {
+		super();
+		this.gameScoreRepository = gameScoreRepository;
+		this.gameScoreKey = gameScoreKey;
+	}
+
+	@GetMapping(path = "/key")
+	public ResponseEntity<String> getKey() {
+		return ResponseEntity.ok(gameScoreKey);
+	}
+
+	@GetMapping(path = "/search")
+	public ResponseEntity<List<Score>> getAll() {
+		return ResponseEntity.ok(gameScoreRepository.findAll());
+	}
 
 }
