643c3b31044dd37153d06bf7805033d062108955
Luis Cortes <luis.cortes@globant.com>
2020-06-23 13:01:55 -0500

    removes unused imports

--- spring-game-score/src/main/java/com/gbt/cloud/springorder/listener/DataInitializer.java
+++ spring-game-score/src/main/java/com/gbt/cloud/springorder/listener/DataInitializer.java
@@ -1,18 +1,19 @@
 package com.gbt.cloud.springorder.listener;
 
-import com.gbt.cloud.springorder.model.Score;
-import com.gbt.cloud.springorder.repository.GameScoreRepository;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.gbt.cloud.springorder.model.Score;
+import com.gbt.cloud.springorder.repository.GameScoreRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @AllArgsConstructor
